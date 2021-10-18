57abb91f5ecc737ac54bcfb3512247e627ee8d2c
rosered <krihsana5@gmail.com>
2021-10-18 13:32:03 +0700

    comment for test release heroku

--- pre-script.sh
+++ pre-script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pwd
-echo $CONFIG_ENCRYPT_KEY | base64 --decode >> ./src/resources/bootstrap.properties
+#echo $CONFIG_ENCRYPT_KEY | base64 --decode >> ./src/resources/bootstrap.properties
